5026b48d3202173fb42a56ae52331f2e490aa251
PGreca <anpgreca@gmail.com>
2018-10-31 10:40:17 +0100

    fix DB

--- controller/pages.php
+++ controller/pages.php
@@ -93,6 +93,7 @@ class pages
 		{
 			if($this->user->data['user_id'] != ANONYMOUS)
 			{
+				echo $this->user->data['user_id'];
 				trigger_error('NO_VIEW_USERS');
 			}
 			login_box('', ((isset($this->user->lang['LOGIN_EXPLAIN_'.strtoupper('viewprofile')])) ? $this->user->lang['LOGIN_EXPLAIN_'.strtoupper('viewprofile')] : $this->user->lang['LOGIN_EXPLAIN_MEMBERLIST']));
