6ee998999b0d283d00abe06b488278104ff1da0e
Mathieu M <Monsieur2M@users.noreply.github.com>
2019-01-09 12:24:36 +0100

    Create permissions_pg_social.php
    First French translation

---
+++ language/fr/permissions_pg_social.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ *
+ * PGreca Social extension for phpBB.
+ *
+ * @copyright (c) 2018 pgreca <https:/pgreca.it>
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+if(!defined('IN_PHPBB'))
+{
+	exit;
+}
+if(empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+$lang = array_merge($lang, array(
+	'ACL_CAT_PG_SOCIAL'		=> 'PG Social',
+	'ACL_U_PAGE_CREATE'		=> 'Créer une page',
+	'ACL_A_PAGE_MANAGE'		=> 'Gérer une page',
+	
+	'ACL_M_PAGE_MANAGE'		=> 'Gérer une page',
+));
